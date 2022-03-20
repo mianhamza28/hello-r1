fb32634d1f3140d7d40c4e86d82ec3dcb2364114
Hamza Shaukat <hamzashaukat28@gmail.com>
2022-03-20 01:56:11 -0700

    few lines added

--- testing.r
+++ testing.r
@@ -5,3 +5,8 @@ name = "hamza"
 print(name)
 print("hello, i am new in R programming can we work together to work more samrtly")
 
+name = "Hamza"
+version = "R studio"
+
+print(name)
+print("This is v efficient")
